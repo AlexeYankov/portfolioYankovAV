d260b899f40d65673ecd1272748bde46c93dcdc7
Alexey Yankov <yankovav.wm@gmail.com>
2023-07-31 11:48:58 +0300

    Updates

---
+++ static/js/871.de770864.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolioYankovAv=self.webpackChunkportfolioYankovAv||[]).push([[871],{871:function(n,o,t){t.r(o),t.d(o,{SmallPicJSX:function(){return i}});var a=t(711),e=t(184),i=function(){return(0,e.jsx)("div",{className:a.Z.mainPhotoContentSmallSizes})};o.default=i}}]);
+//# sourceMappingURL=871.de770864.chunk.js.map
