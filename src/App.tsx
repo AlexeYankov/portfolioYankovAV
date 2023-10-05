08586d312ed72986cec283e0f9921ea1ea807a9c
Alexey Yankov <yankovav.wm@gmail.com>
2023-10-05 12:10:50 +0300

    fixed scroll logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,34 @@ import "./App.css";
 import Header from "./components/header/Header";
 import BodyMain from "./components/body/BodyMain";
 import Footer from "./components/footer/Footer";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [scrollY, setScrollToY] = useState<number>(window.scrollY);
-  const windowScroll = () => {
-    setScrollToY(window.scrollY);
-  };
-
-  useEffect(() => {
-    function setScrollY() {
-      window.addEventListener("scroll", windowScroll);
-    }
+    let userWIndow = window.scrollY;
     if (window.screen.width <= 1440) {
-      setScrollToY(9999);
-    } else {
-      setScrollY();
+        userWIndow = 9999;
     }
-    setScrollY();
-  }, [scrollY]);
+    const [scrollY, setScrollToY] = useState<number>(userWIndow);
+    const windowScroll = () => {
+        setScrollToY(window.scrollY);
+    };
 
-  return (
-    <div className="App">
-      <Header />
-      <BodyMain scrollStatus={scrollY} />
-      <Footer />
-    </div>
-  );
+    useEffect(() => {
+        function setScrollY() {
+            window.addEventListener("scroll", windowScroll);
+        }
+        if (window.screen.width >= 1440) {
+            setScrollY();
+        }
+    }, [scrollY]);
+
+    return (
+        <div className="App">
+            <Header />
+            <BodyMain scrollStatus={scrollY} />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
