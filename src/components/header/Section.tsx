01d7b2f201f09a5ee8c834b64fd40794b4155be8
Alexey Yankov <yankovav.wm@gmail.com>
2023-10-10 14:44:35 +0300

    scroll animation fix

--- src/components/header/Section.tsx
+++ src/components/header/Section.tsx
@@ -15,18 +15,16 @@ export const Section = memo(({sectionName}: SectionNameType) => {
         setPosY(positionY);
     }, []);
     const selectedSection = () => {
-        ref.current.addEventListener("click", function (e) {
+        const barListener = (e: any) => {
             const finalPositionY = Math.floor(posY / 10);
             e.preventDefault();
             smoothScroll(finalPositionY, posY, finalPositionY);
-        });
+        };
+        ref.current.addEventListener("click", barListener);
+        ref.current.removeEventListener("click", barListener);
     };
     return (
-        <a
-            className={styles.headerStyleBar}
-            ref={ref}
-            href={"#" + sectionName}
-            onClick={selectedSection}>
+        <a className={styles.headerStyleBar} ref={ref} href={"#"+sectionName} onClick={selectedSection}>
             {sectionName}
         </a>
     );
